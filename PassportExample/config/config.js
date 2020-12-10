94dccd146ff3c25cfa33e0004399c0e33f977fe5
YeonJuJu <98uplay@naver.com>
2020-12-11 01:49:07 +0900

    Passport Module

---
+++ PassportExample/config/config.js
@@ -0,0 +1,30 @@
+
+/*
+ * 설정
+ */
+
+module.exports = {
+	server_port: 3000,
+	db_url: 'mongodb://localhost:27017/local',
+	db_schemas: [
+	    {file:'./user_schema', collection:'users6', schemaName:'UserSchema', modelName:'UserModel'}
+	],
+	route_info: [
+	],
+	facebook: {		// passport facebook
+		clientID: '1150257705369153',
+		clientSecret: '76281c2374a19b2fb704e848106cad19',
+		callbackURL: 'http://localhost:3000/auth/facebook/callback',
+		profileFields: ['id', 'email', 'name']
+	},
+	twitter: {		// passport twitter
+		clientID: 'id',
+		clientSecret: 'secret',
+		callbackURL: '/auth/twitter/callback'
+	},
+	google: {		// passport google
+		clientID: 'id',
+		clientSecret: 'secret',
+		callbackURL: '/auth/google/callback'
+	}
+}
